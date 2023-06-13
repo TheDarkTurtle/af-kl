5f56c9a86f1dbd581bb548514e7a4c1e6dd5244f
Romain <romain.gilles@360learning.com>
2023-06-13 15:56:43 +0200

    server: mock response

--- server/src/rest/booking-api.ts
+++ server/src/rest/booking-api.ts
@@ -1,10 +1,13 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
 import { BookingDTO } from "./booking-types.js";
+import mock from "./mock.json" assert { type: "json" };
 
 export class BookingAPI extends RESTDataSource {
   override baseURL = "https://my-rest-api.com/";
 
   async getBooking(id: string): Promise<BookingDTO> {
-    return this.get<BookingDTO>(`bookings/${encodeURIComponent(id)}`);
+    //return this.get<BookingDTO>(`bookings/${encodeURIComponent(id)}`);
+
+    return mock as BookingDTO;
   }
 }
