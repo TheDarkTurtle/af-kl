1642dd5cebdd6864f9f7e5e72b3368757b00cf1c
Romain <romain.gilles@360learning.com>
2023-06-13 20:46:32 +0200

    client: login component

---
+++ client/src/app/check-in/check-in.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-check-in',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './check-in.component.html',
+  styleUrls: ['./check-in.component.scss'],
+})
+export class CheckInComponent {}
