6cf1356cd3452121b1bb21d39674855661903e91
Romain <romain.gilles@360learning.com>
2023-06-13 21:49:21 +0200

    client: login form

--- client/src/app/check-in/check-in.component.ts
+++ client/src/app/check-in/check-in.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-check-in',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './check-in.component.html',
   styleUrls: ['./check-in.component.scss'],
 })
-export class CheckInComponent {}
+export class CheckInComponent {
+  loginForm = new FormBuilder().nonNullable.group({
+    code: '',
+    name: '',
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
