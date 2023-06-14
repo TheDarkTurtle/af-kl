da74dc9a60e0d991a329179d80327565fadaa888
Romain <romain.gilles@360learning.com>
2023-06-14 13:52:04 +0200

    client: header title

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
+import { Observable, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +10,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  title = 'Check-in';
+export class HeaderComponent implements OnInit {
+  title$: Observable<string>;
+
+  constructor(private readonly router: Router) {}
+
+  ngOnInit() {
+    this.title$ = this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      map((event) => this.getTitleFromUrl(event.url))
+    );
+  }
+
+  getTitleFromUrl(url: string) {
+    switch (url) {
+      case '/booking':
+        return 'Booking details';
+      default:
+        return 'Check-in';
+    }
+  }
 }
