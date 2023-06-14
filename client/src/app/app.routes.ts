a62539b7bc14abdd52b17e45d239bb2063fdfaca
Romain <romain.gilles@360learning.com>
2023-06-14 13:51:05 +0200

    client: booking component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CheckInComponent } from './check-in/check-in.component';
+import { BookingComponent } from './booking/booking.component';
 
 export const routes: Routes = [
-  { path: 'check-in', component: CheckInComponent },
+  { path: 'booking', component: BookingComponent, title: 'Booking' },
+  { path: 'check-in', component: CheckInComponent, title: 'Check-in' },
   { path: '', redirectTo: '/check-in', pathMatch: 'full' },
 ];
