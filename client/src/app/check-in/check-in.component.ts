f000a1e1e2b29ea2a0302cfd8e8e916a2b1aa451
Romain <romain.gilles@360learning.com>
2023-06-14 18:29:32 +0200

    client: form validation

--- client/src/app/check-in/check-in.component.ts
+++ client/src/app/check-in/check-in.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
 import { BookingService } from '../booking.service';
@@ -13,10 +13,28 @@ import { BookingService } from '../booking.service';
   styleUrls: ['./check-in.component.scss'],
 })
 export class CheckInComponent implements OnInit {
-  loginForm = new FormBuilder().nonNullable.group({
-    code: '',
-    name: '',
-  });
+  loginForm = new FormBuilder().nonNullable.group(
+    {
+      code: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(6),
+          Validators.pattern(/^[2-9a-zA-Z]*$/),
+        ],
+      ],
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+        ],
+      ],
+    },
+    { updateOn: 'blur' }
+  );
 
   error$: Observable<string>;
   isLoading$: Observable<boolean>;
@@ -29,6 +47,18 @@ export class CheckInComponent implements OnInit {
   }
 
   onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
     this.bookingService.retrieveBooking(this.loginForm.getRawValue());
+  }
+
+  get code() {
+    return this.loginForm.controls.code;
+  }
+
+  get name() {
+    return this.loginForm.controls.name;
   }
 }
