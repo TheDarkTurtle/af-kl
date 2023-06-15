5cd152c458d5aa7862da46a47786bb1a44b97f0e
Romain <romain.gilles@360learning.com>
2023-06-15 09:45:39 +0200

    client: booking details

--- client/src/app/booking/booking.component.ts
+++ client/src/app/booking/booking.component.ts
@@ -3,20 +3,28 @@ import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { BookingQuery } from '../__generated__/types';
 import { BookingService } from '../booking.service';
+import { AirportPipe } from '../airport.pipe';
+import { ContactPipe } from '../contact.pipe';
+import { PaxPipe } from '../pax.pipe';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AirportPipe, ContactPipe, PaxPipe],
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss'],
 })
 export class BookingComponent implements OnInit {
   booking$: Observable<BookingQuery['booking'] | null>;
+  showDetails = false;
 
   constructor(private bookingService: BookingService) {}
 
   ngOnInit() {
     this.booking$ = this.bookingService.booking$;
+  }
+
+  toggleDetails() {
+    this.showDetails = !this.showDetails;
   }
 }
